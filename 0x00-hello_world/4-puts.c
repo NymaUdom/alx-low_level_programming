ab594803c642a3f57256f8a29faef099842b7664
NymaUdom <nymamargaret@yahoo.com>
2022-10-13 18:35:45 -0700

    This code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ * main - entry
+ * return: 0 always
+ */
+int main (void)
+{
+
+	puts("/Programming is like building a multilingual puzzle");
+	return(0);
+
+}
---
+++ 4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- Serves as the entry point.
+ *
+ * Return: By default returns zero if no errors otherwise returns errors.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+
+}
