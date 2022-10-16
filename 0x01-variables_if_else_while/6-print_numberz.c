7b13e771b11e4d0cc584df7b0a3f1dd2b390277a
NymaUdom <nymamargaret@yahoo.com>
2022-10-15 21:01:29 -0700

    Keep it simple

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,16 @@
-#include <stdio/h>
+#include <stdio.h>
 /**
- * main - Prints all single digit numbers of base 10 starting from 0,
- * only using putchar and without char variables.
- * Return: Always 0
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: 0
  */
 int main(void)
 {
-	int num;
+	char letter;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
 	putchar('\n');
 	return (0);
 }
