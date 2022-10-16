5c64df6a66b98c6098d23facb1156285f66d468a
NymaUdom <nymamargaret@yahoo.com>
2022-10-15 20:52:26 -0700

    Me

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,14 +2,14 @@
 /**
  * main - Prints the lowercase alphabet in reverse.
  *
- * Return: Always 0
+ * Return: 0
  */
 int main(void)
 {
-	char letters;
+	char letter;
 
 	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letters);
+		putchar(letter);
 	putchar('\n');
 	return (0);
 }
