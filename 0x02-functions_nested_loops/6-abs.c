ce3bcab5ee04271fdc84a024c060d8ad81ef7076
NymaUdom <nymamargaret@yahoo.com>
2022-10-18 21:33:46 -0700

    Tired

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * abs - compute the absolute value of an interger
  * @t: the interger to be computed
  * Return: the absolute value
+ *
  */
 int _abs(int t);
 {
-	if (t >= 0)
-		return (t);
-	else
-		return (-t);
+	int k = 'a';
+	int absvalue;
+
+	if (k < 0)
+		k = k * (-1);
+	absvalue = k;
+	return (absvalue);
 }
