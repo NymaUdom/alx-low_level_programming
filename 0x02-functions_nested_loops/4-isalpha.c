2d35a8c1f4d799809ff47113ebae9abdfe06affc
NymaUdom <nymamargaret@yahoo.com>
2022-10-19 00:11:56 -0700

    Islower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _isalpha - check if charecter id lowercase or uppercase.
+ * _isalpha - check for charecter alphabet
  * @c: character to be checked
  *
  * Return; 1 for uppercase and lowercase, 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
