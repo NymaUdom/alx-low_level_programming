596b84399fbd75f0172d170ec4fd6ea7ab08d658
NymaUdom <nymamargaret@yahoo.com>
2022-10-18 22:25:43 -0700

    putchar

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - print alpha 10 times
+ * print_alphabets - print all alphabet in lowercase
  */
 void print_alphabet_x10(void);
 {
-	char i, k;
+	char letter;
 
-	for (k = 0; k < 10; k++)
-	{
-
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
-	}
+	for (letter 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
 }
