58041b795dfa67e9dfd54e271616709bf9be36ea
NymaUdom <nymamargaret@yahoo.com>
2022-10-24 12:38:21 -0700

    Swap horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int -  function that swaps the values of two integers.
+ * @a: int a
+ * @b: int b
+ */
+void swap_int(int *a, int *b)
+{
+	int j;
+
+	j = *a;
+	*a = *b;
+	*b = j;
+}
