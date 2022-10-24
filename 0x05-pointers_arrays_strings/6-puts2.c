d28b3fc85b6696c537e7a22730347172e182cfeb
NymaUdom <nymamargaret@yahoo.com>
2022-10-24 13:48:17 -0700

    My work

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - print one char out of two
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int = 0;
+
+	for (; str[i] != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
