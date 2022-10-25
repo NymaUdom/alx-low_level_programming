c642cde10d04125b4ec16b776af9645b685be6ff
NymaUdom <nymamargaret@yahoo.com>
2022-10-24 19:56:36 -0700

    Other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,18 +5,20 @@
  */
 void puts_half(char *str)
 {
-	len = 0;
+	int count = 0, i;
 
-	while (*str != '\0')
+	while (count >= 0)
 	{
-		len++;
-		str++;
+		if (str[count] == '\0')
+			break;
+		count++;
 	}
-	str -= (len / 2);
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i =  (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(str[i]);
 	_putchar('\n');
 }
