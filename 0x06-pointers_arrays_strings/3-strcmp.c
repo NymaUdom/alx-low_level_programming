717b04283e2ba1aad6b69915079e1bac9711774b
NymaUdom <nymamargaret@yahoo.com>
2022-10-26 12:42:36 -0700

    Reverse function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  _strcmp - function that compares two strings
+ *  @sl: first parameter
+ *  @s: second parameter
+ *  Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+
+	result = strcmp(s1, s2);
+	return (result);
