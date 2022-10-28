2917db0fb435393528bc6199ec5f14abfc773c0a
NymaUdom <nymamargaret@yahoo.com>
2022-10-27 19:10:12 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 nclude "main.h"
 /**
  * string_toupper -  changes all lowercase letters of a string to uppercase.
- * @s: string parameter
+ * @s: input string
  * Return: pointer to dest
  */
 char *string_toupper(char *s)
@@ -10,7 +10,7 @@ char *string_toupper(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		if (((s + count) >= 97) && ((s + count) <= 122))
+		if  (((s + count) >= 97) && ((s + count) <= 122))
 			*(s + count) = *(s + count) - 32;
 		count++;
 	}
