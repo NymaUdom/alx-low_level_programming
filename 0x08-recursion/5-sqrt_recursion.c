d23380129745e2b328c20f5482c15c0ef7649cbd
NymaUdom <nymamargaret@yahoo.com>
2022-11-02 17:00:11 -0700

    Dare to dream

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input number
+ * Return: square root or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
