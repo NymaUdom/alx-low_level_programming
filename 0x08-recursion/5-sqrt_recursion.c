3bf928d4d1b60a424faf67e8400e07892f348be5
NymaUdom <nymamargaret@yahoo.com>
2022-11-02 17:18:50 -0700

    Dare to dream

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * _sqrt_recursion - returns the natural square root of a number.
+ *  power_operation - returns the natural square root of a number.
  * @n: input number
+ * @c: iterator
  * Return: square root or -1
  */
-int _sqrt_recursion(int n)
+int power_operation(int n, int c)
 {
 	if (c % (n / c) == 0)
 	{
@@ -14,4 +15,17 @@ int _sqrt_recursion(int n)
 			return (-1);
 	}
 	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: input num
+ * Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	if  (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (power_operation(n, 2));
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * is_prime_number -  returns 1 if the input integer is a prime number
+ * @n: input num
+ * Return: natural square root
+ */
+int is_prime_number(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+
