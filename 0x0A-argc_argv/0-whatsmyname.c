f719e5cd1591d505801de4fee8d60d956f422e6d
NymaUdom <nymamargaret@yahoo.com>
2022-11-04 17:04:32 -0700

    Header file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the name of the program
+ * @argc: number of argument pass through the function
+ * @argv: argument vector from pointer to string
+ *
+ * Return: 0
+ */
+int main(argc_attribute_((unused)). char *argv[])
+{
+	printf("%s\n" argv[])
+		return (0);
+}
