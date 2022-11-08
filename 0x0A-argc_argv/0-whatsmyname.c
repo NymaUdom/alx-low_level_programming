fd94628d07c36fe6862d5a458805e1c794f565a7
NymaUdom <nymamargaret@yahoo.com>
2022-11-08 15:32:26 -0800

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main - function to print it's name
- * @argc: argc parameter
- * @argv: an array of a command listed
- *
- * Return: 0
+ * main - returns the name of a program
+ * @argc: length of array
+ * @argv:  members of the array
+ * Return: success
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv[]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
