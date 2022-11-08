ca63f67008c15f2e8cb6e3641ae88120f80a9af2
NymaUdom <nymamargaret@yahoo.com>
2022-11-08 13:52:07 -0800

    woman with no imagination

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,26 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
- * _strdup - returns a pointer to a new string
- * which is a duplicate of the string
- * @str: char
- *Return: NULL if str is NUL
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string
+ *Return: strout
  */
 char *_strdup(char *str)
 {
-	char *aaa;
-	int i, r = 0;
+	char *strout;
+	unsigned int i, j;
 
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	aaa = malloc(sizeof(char) * (i + 1));
-	if (aaa == NULL)
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+	if (strout == NULL)
 		return (NULL);
-	if (r = 0, str[r], r++)
-		aaa[r] = str[r];
-	return (aaa);
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
+	return (strout);
 }
